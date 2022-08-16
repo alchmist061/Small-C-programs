712ae6a64993500bb50b649fe16d07f8eb97bf1d
alchemist <108540004+alchmist061@users.noreply.github.com>
2022-08-16 17:19:01 +0545

    Add files via upload

---
+++ cap.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define LIMIT 102
+
+char *capital(char *);
+
+
+int main(int argc, char **argv){
+    char *str;
+    str = (char *) malloc(sizeof(char)*100+1);
+    puts("Enter your string: ");
+    fgets(str,LIMIT,stdin);
+    printf("your string in capital is:\n%s",capital(str));
+    free(str);
+    return 0;
+}
+
+char *capital(char *arr){
+    int b=0;
+    while(*(arr+b) != '\0'){
+        if(*(arr+b)>=97 && *(arr+b) <= 122)
+            *(arr+b) -= 32;
+        else
+            *(arr+b) = *(arr+b);
+        b++;
+    }
+    return arr;
+}
+
+
